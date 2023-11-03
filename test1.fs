1334b192c17f761d7050fcf2f75886a7a103c31c
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2023-11-03 17:43:42 +0100

    WIP

--- test1.fs
+++ test1.fs
@@ -1,15 +1,15 @@
 s" tmp.php" w/o create-file throw Value fd-out
 
 : startphp ( -- )
-    s\" <?php\n" fd-out write-line
+    s" <?php" fd-out write-line
     ;
 : w ( )
     fd-out write-file
     ;
 : echo
-    s\" echo \"" w drop
+    s" echo '" w drop
     w drop
-    s\" \";\n" w
+    s" ';" fd-out write-line
     ;
 : endphp fd-out close-file throw ;
 
