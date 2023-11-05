0a961e4de4597a0614699cece1cdffa2b77ce445
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2023-11-05 17:09:27 +0100

    WIP

--- test2.fs
+++ test2.fs
@@ -1,10 +1,10 @@
-vocabulary php_args
+vocabulary php_args \ The php_args word list is open only when writing down arguments to a function
 php_args also
 definitions
-order cr
+\ order cr
 : string "string" type ;
 previous Forth definitions
-order cr
+\ order cr
 
 : <?php "<?php" type cr cr ;
 
@@ -12,12 +12,13 @@ order cr
     >in @ create >in ! here >r bl parse dup >r dup c, here swap cmove r> 1+ allot
     s" function " type r> count type s" (" type
     php_args also
-    order cr
+    \ order cr
     does> count type s" ()" type ;
 
 : returns 
     parse-name "): " type type cr "{" type cr 
-    previous previous order cr
+    previous previous
+    \ order cr
 ;
 
 : endfn "}" type cr ;
@@ -33,6 +34,7 @@ order cr
 <?php
 
 fn foo
+\ TODO: string $html or assume $payload, always only one argument and always `mixed`?
 returns string
 endfn
 
